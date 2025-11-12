36a62968835267b1a099a0a534008ad232d41d53
amacken3 <aengus.2000@gmail.com>
2025-11-12 15:24:44 -0500

    added number display for cart and css for navbar as well as updated navbar to be more responsive

--- card-shop-client/src/components/Navbar.js
+++ card-shop-client/src/components/Navbar.js
@@ -1,15 +1,31 @@
+
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
+import "../styles/navbar.css"; // make sure this line exists
 
 function Navbar() {
+  const { cart } = useContext(CartContext);
+
+  // total number of items across the whole cart
+  const itemCount = cart.reduce((sum, item) => sum + (item.quantity || 0), 0);
+
   return (
-    <nav>
-      <ul style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+    <nav className="navbar">
+      <ul className="nav-list">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/shop">Shop</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
+        <li className="nav-cart">
+          <Link to="/cart">
+            Cart
+            {itemCount > 0 && <span className="cart-badge">{itemCount}</span>}
+          </Link>
+        </li>
+        <li><Link to="/checkout">Checkout</Link></li>
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
+
